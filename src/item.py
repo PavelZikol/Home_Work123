c5345e4c3ada2f4b629ee6b5006ff0556dd4deab
PavelZikol <142353611+PavelZikol@users.noreply.github.com>
2023-09-25 23:57:48 +0400

    fixed item

--- src/item.py
+++ src/item.py
@@ -16,9 +16,50 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        super().__init__()
+        self.__name = name
         self.price = price
         self.quantity = quantity
+
+    def __repr__(self):
+        return f"Item({repr(self.__name):str}, {repr(self.price):float}, {repr(self.quantity):int})"
+
+    def __str__(self):
+        return f"{self.__name}"
+
+    def __add__(self, other):
+        if issubclass(other.__class__, self.__class__):
+            total_sum = self.quantity + other.quantity
+            return total_sum
+        return f"Sorry, bro)"
+
+    @classmethod
+    def instantiate_from_csv(cls, file):
+        """открытие csv и заполнение пусого списка all"""
+        cls.all = []
+        with open(file) as f:
+            reader = csv.DictReader(f)
+            for i in reader:
+                cls.all.append(cls(i["name"], i["price"], i["quantity"]))
+
+
+    @staticmethod
+    def string_to_number(string_num):
+        """Возврат чисел из строки"""
+        if string_num.isdigit():
+            return int(string_num)
+        return float(string_num) // 1
+
+    @property
+    def name(self):
+        """гетер"""
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        """поменять имя"""
+        if 0 < len(new_name) < 10:
+            self.__name = new_name[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -34,45 +75,6 @@ class Item:
         """
         self.price *= self.pay_rate
 
-    @property
-    def name(self):
-        """гетер"""
-        return self.__name
 
-    @name.setter
-    def name(self, new_name):
-        """поменять имя"""
-        if 0 < len(new_name) < 10:
-            self.__name = new_name[:10]
-
-    @classmethod
-    def instantiate_from_csv(cls, file):
-        """открытие csv и заполнение пусого списка all"""
-        cls.all = []
-        with open(file) as f:
-            reader = csv.DictReader(f)
-            for i in reader:
-                cls.all.append(cls(i["name"], i["price"], i["quantity"]))
-
-    @staticmethod
-    def string_to_number(num):
-        """Возврат чисел из строки"""
-        return int(float(num))
-
-    def __add__(self, other):
-        if isinstance(other, Item):
-            return Item(self.quantity + other.quantity)
-
-    def __repr__(self):
-        return f"Item({repr(self.__name):str}, {repr(self.price):float}, {repr(self.quantity):int})"
-
-    def __str__(self):
-        return f"{self.__name}"
-
-    def __add__(self, other):
-        if issubclass(other.__class__, self.__class__):
-            total_sum = self.quantity + other.quantity
-            return total_sum
-        return f"Sorry, bro)"
 
 
